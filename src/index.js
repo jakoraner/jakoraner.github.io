3972fcf590e13bca2f1cb9885479cd3407f45b9d
Jacob Hoffmann <122924617+jakoraner@users.noreply.github.com>
2024-11-12 15:07:59 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; // Ensure Tailwind CSS styles are included
-import App from './app';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
