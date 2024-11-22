55594c8a8dc620162596880a2178fcf6d373fbb8
lc4415 <lc4415@partner.kit.edu>
2024-11-22 18:31:42 +0100

    redirects

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/Landingpage'; // Adjust based on your structure
-import SpotifyPlaylists from './components/Playlists'; // Adjust based on your structure
+import Playlists from './components/Playlists'; // Adjust based on your structure
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/playlists" element={<SpotifyPlaylists />} />
+        <Route path="/playlists" element={<Playlists />} />
       </Routes>
     </Router>
   );
